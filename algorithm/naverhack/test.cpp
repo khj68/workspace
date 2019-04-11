09a83a0ef5480c22b18f7cb680c5cf515a42b398
khj68 <rlagudwns68@gmail.com>
2019-04-11 21:48:06 +0900

    add test code

--- algorithm/naverhack/test.cpp
+++ algorithm/naverhack/test.cpp
@@ -1,9 +1,38 @@
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
+int arr[100001];
+
+int solution(vector<int> &A) {
+  for(auto a: A){
+    // printf("%d\n", a);
+    arr[a]++;
+  }
+
+  // for(int i=1; i<A.size(); i++){
+  //   // printf("%d\n", arr[i]);
+  //   if(arr[i] == 0){
+  //     printf("%d\n", i);
+  //     break;
+  //   }
+  // }
+}
+
+
 int main(){
-  printf("test\n");
+  vector<int> a = {1,2,3};
+  solution(a);
+  int i=1;
+  while(true){
+    // printf("%d\n", arr[i]);
+    if(arr[i] == 0){
+      printf("%d\n", i);
+      break;
+    }
+    i++;
+  }
 
   return 0;
 }
